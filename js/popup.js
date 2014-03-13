326cb885ef76f7e8d8589a1a73561ebf5b3d8bb4
Zachary McCord <zjmccord@gmail.com>
2014-03-13 14:56:29 -0400

    Successful communication between extension components

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,18 @@
 window.addEventListener("load", function(){
-    if (!window.hasOwnProperty('rebloguard')) {
-        window.rebloguard = {};
-    }
-    var rebloguard = window.rebloguard;
+    var bgpage = chrome.extension.getBackgroundPage();
+    var rebloguard = window.rebloguard = bgpage.rebloguard;
 
     if (!rebloguard.hasOwnProperty('popup')) {
         rebloguard.popup = {};
     }
     var popup = rebloguard.popup;
 
+
     var div = popup.div = document.getElementById('rebloguard-popup');
     console.log("Popup div", div);
 
-    var button = popup.button = document.getElementById('rebloguard-disablebutton');
-    button.onclick = function() {
-        alert("click");
+    var stat = popup.stat = document.getElementById('rebloguard-status');
+    if (rebloguard.initialized) {
+        stat.textContent = "Rebloguard is up and running";
     }
 }, true);
